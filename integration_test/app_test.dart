66dae44b0977d5ba33341092b91adfcfa3561dbe
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-12-06 19:30:38 +0900

    ワークフローで結合試験実施し、スクショ撮って後で確認できるように処理追加

---
+++ integration_test/app_test.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import '../lib/main.dart' as app;
+
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      await takeScreenshot(tester, binding, 'shot-1');
+
+      // Finds the floating action button to tap on.
+      final Finder fab = find.byTooltip('Increment');
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+      await takeScreenshot(tester, binding, 'shot-2');
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+    });
+  });
+}
+
+takeScreenshot(tester, binding, name) async {
+  if (Platform.isAndroid) {
+    try {
+      await binding.convertFlutterSurfaceToImage();
+    } catch (e) {
+      print("TakeScreenshot exception $e");
+    }
+    await tester.pumpAndSettle();
+  }
+
+  await binding.takeScreenshot(name);
+}
---
+++ test_driver/main_test.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:integration_test/integration_test_driver.dart';
+
+Future<void> main() async {
+  try {
+    await integrationDriver(
+      responseDataCallback: (Map<String, dynamic>? x) async {
+        if (x == null || x.isEmpty) return;
+
+        late Map<String, List<int>> nameByteDict;
+        if (x is Map<String, List<int>>) {
+          nameByteDict = x;
+        }
+
+        nameByteDict.forEach((key, value) async {
+          final File image =
+              await File('screenshots/$key.png').create(recursive: true);
+          image.writeAsBytesSync(value);
+        });
+        return;
+      },
+    );
+  } catch (e) {
+    print('onScreenshot - error - $e');
+  }
+}
